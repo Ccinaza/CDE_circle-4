a7f59c0d567544dab3b5fe1d6de54d2d5931a51b
iamthedarksaint <bojzino128@gmail.com>
2025-09-09 00:02:46 +0100

    Add a new script

--- git_linux_assignment/scripts/sql/parch_and_posey_eda.sql
+++ git_linux_assignment/scripts/sql/parch_and_posey_eda.sql
@@ -1,4 +1,4 @@
--- From the accounts table, which sales rep has the most customers under them
+-- 1. From the accounts table, which sales rep has the most customers under them
 select s.name as sales_rep_name, count(*) as total_customers
 from accounts a
 join sales_reps s 
@@ -6,7 +6,7 @@ on s.id = a.sales_rep_id
 group by 1
 order by total_customers desc;
 
--- Show the yearly performance of each region in terms of both order volume and total order value
+-- 2. Show the yearly performance of each region in terms of both order volume and total order value
 
 SELECT r.name AS region,
        DATE_PART('year', o.occurred_at) AS order_year,
@@ -19,3 +19,12 @@ JOIN region r ON s.region_id = r.id
 GROUP BY r.name, order_year
 ORDER BY order_year, total_orders DESC, total_order_value DESC;
 
+--3. top 10 accounts by total order amount in USD
+
+SELECT a.name, sum(o.total) total_qty, sum(o.total_amt_usd) total_amt_usd
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+GROUP BY 1
+ORDER BY 3 DESC
+LIMIT 10;
